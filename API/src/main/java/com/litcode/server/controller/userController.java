bfe67d843b80a36077374cbfcdb61340a0865d51
xymon2 <vbbn2030@gmail.com>
2022-10-20 18:04:03 +0900

    FEAT: add dependencies to use jpa

--- API/src/main/java/com/litcode/server/controller/userController.java
+++ API/src/main/java/com/litcode/server/controller/userController.java
@@ -1,22 +1,33 @@
 package com.litcode.server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
+import com.litcode.server.service.UserService;
+
+class signInInfo{
+  public String id;
+  public String password;
+}
 
 @RestController
-@RequestMapping(controllerUtils.prefix + "/users")
-public class userController {
+@RequestMapping(ControllerUtils.prefix + "/users")
+public class UserController {
+	@Autowired
+	private UserService userService;
 
   @PostMapping("/sign-in")
-  private String signIn() {
-    throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED, "WAIT");
+  public String signIn(@RequestBody signInInfo signInInfo) {
+    String ret = userService.signIn();
+    return ret;
   }
 
   @PostMapping("/")
-  private String signUp() {
+  public String signUp() {
     throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED, "WAIT");
   }
 }
