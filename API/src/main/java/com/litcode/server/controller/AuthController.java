786bbcbe689a4e6e830d7a2ba0fb73ede87286f5
xymon2 <vbbn2030@gmail.com>
2022-10-26 18:19:49 +0900

    REFACTOR: clean up and add comments to auth controller

--- API/src/main/java/com/litcode/server/controller/AuthController.java
+++ API/src/main/java/com/litcode/server/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.litcode.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.litcode.server.dto.SignInRequest;
 import com.litcode.server.security.JwtTokenProvider;
@@ -26,14 +29,24 @@ public class AuthController {
 	@PostMapping("")
 	public String signIn(@RequestBody SignInRequest signInInfo) {
 
-		Authentication authentication = authenticationManager
-				.authenticate(
-						new UsernamePasswordAuthenticationToken(signInInfo.getId(),
-								signInInfo.getPassword()));
+		try {
+			// Check id and password are valid
+			Authentication authentication = authenticationManager
+					.authenticate(
+							new UsernamePasswordAuthenticationToken(signInInfo.getId(),
+									signInInfo.getPassword()));
 
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String jwtToken = jwtTokenProvider.generateToken(signInInfo.getId());
+			// Save authentication info to security context
+			// And make jwt token
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			String jwtToken = jwtTokenProvider.generateToken(signInInfo.getId());
+			return jwtToken;
 
-		return jwtToken;
+		} catch (BadCredentialsException ex) {
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "WRONG_USER_INFO");
+		} catch (Exception ex) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR");
+		}
+
 	}
 }
