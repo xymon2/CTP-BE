de8e7352660ee8f8664713ce67906f7aca1a3919
xymon2 <vbbn2030@gmail.com>
2022-11-14 20:17:00 +0900

    REFACOR: refactor problem summary(get all)

--- DB/script/seed.sql
+++ DB/script/seed.sql
@@ -3,7 +3,38 @@ SET GLOBAL TIME_ZONE = 'Asia/Seoul';
 
 INSERT INTO users (id,user_id,name,email,password,solved_id,created,updated) VALUE (1,'xymon2','성빈','test@email.com','$2y$10$aWO50OhBjHY2ePv5J47fWuxV8jZo0jntOssEsk5lmkUhU8Edr9s.S','[1]','2021-01-14 15:52:44','2021-01-14 15:52:44');
 
-INSERT INTO problems (id,name,description,sample_input) VALUE (1,'deepcopy','deepcopyproblem','{"a":1,"b":2}');
+INSERT INTO problems (id,name,description,sample_input) VALUE (1,'deepcopy','
+## Deepcopy
+
+There are parsed Json objects.  
+Implement a function deepcopying the objects.  
+Input object should not be changed even if output object has changed.
+
+```js
+const input = {
+    "a" = 1;
+};
+
+const output = deepcopy(input);
+output.a = 0;
+
+console.log(input.a); // should be 1
+console.log(output.a) // should be 0
+```
+
+**Sample inputs**
+
+```bash
+# sample input 1
+3
+
+# sample input 2
+["3", 1, "Hello", null]
+
+# sample input 3
+{"key" : [{"foo" : "bar"}]}
+```
+','{"a":1}');
 INSERT INTO skeleton_code(id,problem_id,language,code) VALUE (1,1,'JavaScript','JS solution');
 INSERT INTO skeleton_code(id,problem_id,language,code) VALUE (2,1,'Python3','Python3 solution');
 INSERT INTO skeleton_code(id,problem_id,language,code) VALUE (3,1,'Java','Python3 solution');
